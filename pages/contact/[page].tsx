dbc926bfb387aacba3f5ed8ebcbc0e1b15e880b0
Andres Hernandez <andreshernandez12.1995@gmail.com>
2024-11-13 20:09:19 +0100

    Add sass prettier shadcn and first layout

--- pages/contact/[page].tsx
+++ pages/contact/[page].tsx
@@ -1,6 +1,6 @@
-import { useRouter } from 'next/router'
- 
+import { useRouter } from "next/router";
+
 export default function Page() {
-  const router = useRouter()
-  return <p>Post: {router.query.page}</p>
+  const router = useRouter();
+  return <p>Post: {router.query.page}</p>;
 }
